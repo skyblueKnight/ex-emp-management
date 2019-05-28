c4efb8a5693131a7fc350592f6ebdd8bececf91b
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-05-28 14:11:47 +0900

    管理者コントローラを作成しました。

---
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -0,0 +1,43 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+/**
+ * 管理者関係のコントローラ.
+ * 
+ * @author momoyo kanie
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+
+	@Autowired
+	private AdministratorService administratorService;
+
+	/**
+	 * 管理者フォームをインスタンス化する.
+	 * 
+	 * @return 管理者フォーム
+	 */
+	@ModelAttribute
+	public InsertAdministratorForm setUpInsertAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+
+	/**
+	 * 管理者登録画面に遷移する.
+	 * 
+	 * @return 管理者登録画面
+	 */
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "/administrator/insert";
+	}
+}
