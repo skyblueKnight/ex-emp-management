44508cc44ec68fcf00380c27578005b660c967a9
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-05-28 16:10:35 +0900

    ログイン処理時のエラー処理をBindingResultに変更しました。

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -25,7 +27,7 @@ public class AdministratorController {
 
 	@Autowired
 	private AdministratorService administratorService;
-	
+
 	@Autowired
 	private HttpSession session;
 
@@ -55,7 +57,7 @@ public class AdministratorController {
 	 * @return ログイン画面
 	 */
 	@RequestMapping("/")
-	public String toLogin() {
+	public String toLogin(Model model) {
 		return "administrator/login";
 	}
 
@@ -83,23 +85,28 @@ public class AdministratorController {
 
 		return "redirect:/";
 	}
-	
-	
+
+	/**
+	 * ログイン処理を行う.<br>
+	 * ログインが成功したら従業員リストページに移動し、 ログインに失敗したらエラーメッセージを格納して、ログイン画面に戻る。
+	 * 
+	 * @param loginForm 入力されたログイン情報
+	 * @param model     モデル
+	 * @return 成功時/従業員リストページ 失敗時/ログイン画面
+	 */
 	@RequestMapping("/login")
-	public String login(LoginForm loginForm, Model model) {
-		
+	public String login(@Validated LoginForm loginForm, BindingResult result, Model model) {
+
 		Administrator administrator = administratorService.login(loginForm.getMailAddress(), loginForm.getPassword());
-		
-		if(administrator != null) {			
-			session.setAttribute("administratorName", administrator.getName());			
-		}else {
-			model.addAttribute("errorMessage","メールアドレスまたはパスワードが不正です。");
+
+		if (administrator == null) {
+			result.rejectValue(loginForm.getMailAddress(), null, "メールアドレスまたはパスワードが不正です。");
+			return toLogin(model);
 		}
 
+		session.setAttribute("administratorName", administrator.getName());
 		return "forward:/employee/showList";
-		
+
 	}
-	
-	
 
 }
