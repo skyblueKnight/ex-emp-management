646eec870e82e9d85005fa82ba7bb8ca885bc2b4
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-05-29 09:52:48 +0900

    従業員詳細情報表示画面を作成しました。

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 /**
@@ -23,6 +25,17 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
+	
+	/**
+	 * 従業員情報更新時に使用するフォームをインスタンス化する.
+	 * 
+	 * @return 従業員情報更新時に使用するフォーム
+	 */
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		return new UpdateEmployeeForm();
+	}
+	
 
 	/**
 	 * 従業員一覧を出力する.
@@ -32,12 +45,27 @@ public class EmployeeController {
 	 */
 	@RequestMapping("/showList")
 	public String showList(Model model) {
-		List<Employee> employeeList = new ArrayList<>();
-		employeeList = employeeService.showList();
+		List<Employee> employeeList = employeeService.showList();
 		System.out.println(employeeList);
 		model.addAttribute("employeeList", employeeList);
 
 		return "employee/list";
+	}
+	
+	
+	/**
+	 * 選択されたIDから従業員情報を取得して出力する.
+	 * 
+	 * @param id 選択されたID
+	 * @param model モデル
+	 * @return 従業員情報詳細表示画面
+	 */
+	@RequestMapping("/showDetail")
+	public String showDetail(String id, Model model) {
+		Employee employee = employeeService.showDetail(Integer.parseInt(id));
+		model.addAttribute("employee",employee);
+		
+		return "employee/detail";		
 	}
 
 }
